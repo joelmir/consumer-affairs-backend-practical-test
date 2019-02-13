d443c629eec994de438af02167329bd061337aed
Joelmir Ribacki <joedekid@gmail.com>
2019-02-12 22:44:35 -0200

    added filter to admin if is not super user just show the reviews from the user logged

--- review/admin.py
+++ review/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
+from rest_framework.authtoken.models import Token
 from review.models import Company
 from review.models import Review
 
+class ReviewAdmin(admin.ModelAdmin): 
+    def get_queryset(self, request): 
+        qs = super(ReviewAdmin, self).get_queryset(request) 
+        if not request.user.is_superuser:
+            return qs.filter(reviewer=Token.objects.get(user=request.user))
+        else:
+            return qs
+
+
 admin.site.register(Company)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
